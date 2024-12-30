7141fba23581528f52a79cff8b09809b021303be
mrh-smil3 <arifhidayanto860@gmail.com>
2024-12-30 10:29:04 +0700

    update: logo blog

--- resources/views/blog_public/layouts/main.blade.php
+++ resources/views/blog_public/layouts/main.blade.php
@@ -10,13 +10,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
     <!-- Site Metas -->
-    <title>Nex-Gen Blog</title>
-    <meta name="keywords" content="" />
-    <meta name="description" content="" />
-    <meta name="author" content="" />
+    <title>Nexgen Blog - Buat Website Mudah Wordpress</title>
+    <meta name="keywords" content="Nexgen, blog, WordPress, website UMKM, pembuatan website, jasa website, desain web, website jepara, website furniture, website rattan, company profile, portofolio, Buat Website Mudah" />
+    <meta name="description" content="Nexgen Blog adalah sumber informasi tentang pembuatan website WordPress dan layanan untuk UMKM. Temukan tips, panduan, dan solusi untuk meningkatkan bisnis Anda secara online." />
+    <meta name="author" content="Nexgen Team" />
 
-    <!-- Site Icons -->
-    <link rel="shortcut icon" href="{{ asset('/storage/logo/icons_nex_gen.png') }}" type="image/x-icon" />
+    <!-- Open Graph Meta -->
+    <meta property="og:title" content="Nexgen Blog - WordPress & Website UMKM, Buat Website Mudah, Jasa buat website, jasa pembuatan website profesional" />
+    <meta property="og:description" content="Nexgen Blog menyajikan tips dan panduan pembuatan website WordPress untuk UMKM. Solusi terbaik untuk bisnis online Anda!" />
+    <meta property="og:image" content="{{ asset('storage/logo/nexgen-header.png') }}" />
+    <meta property="og:url" content="https://nex-gen.id/blog" />
+    <meta property="og:type" content="website" />
+
+    <!-- Twitter Card -->
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:title" content="Nexgen Blog - WordPress & Website UMKM, Buat Website Mudah, Jasa buat website, jasa pembuatan website profesional" />
+    <meta name="twitter:description" content="Temukan tips, panduan, dan solusi terbaik untuk pembuatan website WordPress bagi UMKM di Nexgen Blog!" />
+    <meta name="twitter:image" content="{{ asset('storage/logo/nexgen-icon.png') }}" />
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="{{ asset('storage/logo/nexgen-icon.png') }}" type="image/x-icon" />
     <link rel="apple-touch-icon" href="images/apple-touch-icon.png" />
 
     <!-- Design fonts -->
@@ -24,32 +37,22 @@
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,400i,500,700,900" rel="stylesheet" />
 
     <!-- Bootstrap core CSS -->
-    {{-- <link href="css/bootstrap.css" rel="stylesheet" /> --}}
     <link href="{{ asset('blog/css/bootstrap-minls.css') }}" rel="stylesheet" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
     <!-- FontAwesome Icons core CSS -->
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link href="{{ asset('blog/css/font-awesome.min.css') }}" rel="stylesheet" />
 
     <!-- Custom styles for this template -->
-    {{-- <link href="style.css" rel="stylesheet" /> --}}
     <link href="{{ asset('blog/css/style.css') }}" rel="stylesheet" />
-
-    <!-- Responsive styles for this template -->
-    {{-- <link href="css/responsive.css" rel="stylesheet" /> --}}
     <link href="{{ asset('blog/css/responsive.css') }}" rel="stylesheet" />
-
-    <!-- Colors for this template -->
-    {{-- <link href="css/colors.css" rel="stylesheet" /> --}}
     <link href="{{ asset('blog/css/colors.css') }}" rel="stylesheet" />
 
 </head>
 
 <body>
-
     <div id="wrapper">
         <div class="collapse top-search" id="collapseExample">
             <div class="card card-block">
@@ -61,31 +64,18 @@
                         </button>
                     </form>
                 </div>
-                <!-- end newsletter -->
             </div>
         </div>
-        <!-- end top-search -->
 
-        {{-- topbar section --}}
         @include('blog_public.partials.topbar')
-        {{-- end topbar --}}
-
-
-        {{-- header section --}}
         @include('blog_public.partials.header')
-        {{-- endheader --}}
-
         @yield('main')
-
-
     </div>
-    <div class=" bottom-0" style="margin-top: 150px">
-        {{-- section footer --}}
+
+    <div class="bottom-0" style="margin-top: 150px">
         @include('blog_public.partials.footer')
     </div>
 
-
-    {{-- <script src="js/jquery.min.js"></script> --}}
     <script src="{{ asset('blog/js/jquery.min.js') }}"></script>
     <script src="{{ asset('blog/js/tether.min.js') }}"></script>
     <script src="{{ asset('blog/js/bootstrap.min.js') }}"></script>
